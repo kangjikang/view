007b67b487ee68f44e0d046f56950f7ab35fc84e
KangJi <kjhmd9726@gmail.com>
2024-05-18 12:22:19 +0900

    resource fix

--- data/profileInfoList.js
+++ data/profileInfoList.js
@@ -1,101 +1,100 @@
-import profileKeke from '@/public/profile/keke.png';
-import profile강혜원 from '@/public/profile/강혜원.png';
-import profile김채원 from '@/public/profile/김채원.png';
-import profile사나 from '@/public/profile/사나.png';
-import profile설윤 from '@/public/profile/설윤.png';
-import profile설현 from '@/public/profile/설현.png';
-import profile수지 from '@/public/profile/수지.png';
-import profile아이유 from '@/public/profile/아이유.png';
-import profile안유진 from '@/public/profile/안유진.png';
-import profile우기 from '@/public/profile/우기.png';
-import profile윈터 from '@/public/profile/윈터.png';
-import profile윤아 from '@/public/profile/윤아.png';
-import profile장원영 from '@/public/profile/장원영.png';
-import profile지수 from '@/public/profile/지수.png';
-import profile최예나 from '@/public/profile/최예나.png';
-import profile카리나 from '@/public/profile/카리나.png';
-import profile해린 from '@/public/profile/해린.png';
+import profile개발자 from '@/public/profile/개발자.png';
+import profile김영광 from '@/public/profile/김영광.png';
+import profile박보검 from '@/public/profile/박보검.png';
+import profile서강준 from '@/public/profile/서강준.png';
+import profile손석구 from '@/public/profile/손석구.png';
+import profile송강 from '@/public/profile/송강.png';
+import profile안효섭 from '@/public/profile/안효섭.png';
+import profile우도환 from '@/public/profile/우도환.png';
+import profile유연석 from '@/public/profile/유연석.png';
+import profile이도현 from '@/public/profile/이도현.png';
+import profile이동욱 from '@/public/profile/이동욱.png';
+import profile이진욱 from '@/public/profile/이진욱.png';
+import profile정경호 from '@/public/profile/정경호.png';
+import profile조정석 from '@/public/profile/조정석.png';
+import profile채종협 from '@/public/profile/채종협.png';
+import profile홍경 from '@/public/profile/홍경.png';
 
 const profileInfoList = [
   {
     idx: 1,
-    name: '강혜원',
-    image: profile강혜원,
+    name: '개발자',
+    image:profile개발자,
   },
   {
     idx: 2,
-    name: '김채원',
-    image: profile김채원,
+    name: '김영광',
+    image:profile김영광,
   },
   {
     idx: 3,
-    name: '사나',
-    image: profile사나,
+    name: '박보검',
+    image:profile박보검,
   },
   {
     idx: 4,
-    name: '설윤',
-    image: profile설윤,
+    name: '서강준',
+    image:profile서강준,
   },
   {
     idx: 5,
-    name: '설현',
-    image: profile설현,
+    name: '손석구',
+    image:profile손석구,
   },
   {
     idx: 6,
-    name: '수지',
-    image: profile수지,
+    name: '송강',
+    image:profile송강,
   },
   {
     idx: 7,
-    name: '아이유',
-    image: profile아이유,
+    name: '안효섭',
+    image:profile안효섭,
   },
   {
     idx: 8,
-    name: '안유진',
-    image: profile안유진,
+    name: '우도환',
+    image:profile우도환,
   },
   {
     idx: 9,
-    name: '우기',
-    image: profile우기,
+    name: '유연석',
+    image:profile유연석,
   },
   {
     idx: 10,
-    name: '윈터',
-    image: profile윈터,
+    name: '이도현',
+    image:profile이도현,
   },
   {
     idx: 11,
-    name: '윤아',
-    image: profile윤아,
+    name: '이동욱',
+    image:profile이동욱,
   },
   {
     idx: 12,
-    name: '장원영',
-    image: profile장원영,
+    name: '이진욱',
+    image:profile이진욱,
   },
   {
     idx: 13,
-    name: '지수',
-    image: profile지수,
+    name: '정경호',
+    image:profile정경호,
   },
   {
     idx: 14,
-    name: '최예나',
-    image: profile최예나,
+    name: '조정석',
+    image:profile조정석,
   },
   {
     idx: 15,
-    name: '카리나',
-    image: profile카리나,
+    name: '채종협',
+    image:profile채종협,
   },
   {
     idx: 16,
-    name: '해린',
-    image: profile해린,
+    name: '홍경',
+    image:profile홍경,
   },
 ];
 
